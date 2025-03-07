540c489918b320c0d4ad914368cc8ea654b77ab6
Tarani Yaragani <147273506+Tarani12@users.noreply.github.com>
2025-03-07 19:59:30 +0530

    Delete speech_therapy directory

--- speech_therapy/script1.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const contactBtn = document.querySelector(".contact-btn");
-
-    contactBtn.addEventListener("click", function () {
-        alert("Thank you for reaching out! We will contact you soon.");
-    });
-});
